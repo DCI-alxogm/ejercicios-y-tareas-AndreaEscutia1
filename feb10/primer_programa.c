d89e97806cfd0b64868d47cc66b031316c6a3cfa
AndreaEscutia1 <a.escutiazamudio@ugto.mx>
2025-02-10 11:38:17 -0600

    guardado primer programa

---
+++ feb10/primer_programa.c
@@ -0,0 +1,9 @@
+/*creado el 10 de febrero por andrea 2025*/
+#include<stdio.h>
+int main()
+{
+printf("hola andrea\n");
+printf("este es el primer programa del curso\n");
+}
+/*termine de escribir el programa*/
+
