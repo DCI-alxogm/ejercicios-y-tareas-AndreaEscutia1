cfbca4dbaab063e7dd8d4dc4d1a2c7281e5ce1bf
AndreaEscutia1 <a.escutiazamudio@ugto.mx>
2025-02-23 11:59:32 -0600

    corregir

--- feb19/coordenadas.c
+++ feb19/coordenadas.c
@@ -8,26 +8,28 @@
 
 int main(){
 float x, y, z; //variables principales
-float re, rc, tetha, phit, z; //variables por calcular
+float re, rc, tetha, phi; //variables por calcular
 // cuerpo del programa
+printf("conversion de coordenadas cartesianas (x,y,z)\n");
+printf("a coordenadas esfericas (re,tetha,phi) y cilindricaas (rc,tetha,z)\n");
 printf("ingrese coordenadas para x,y,z: \n");
 scanf("%f%f%f", &x, &y, &z);
 //conversion a coordenadas esfericas
-re = sqrt(x*x + y*y + z*z) //radio
-theta = atan2(x,y); //theta (angulo)
-phi = acos(z/re) // angulo respecto a z
+re = sqrt(x*x + y*y + z*z); //radio
+tetha = atan2(x,y); //tetha (angulo)
+phi = acos(z/re); // angulo respecto a z
 
 //conversion a coordenadas cilindricas
 rc = sqrt(x*x + y*y); //radio cilindrico
 //z es igual
-//theta ya se calculo
+//tetha ya se calculo
 printf("resultados \n");
 //resultados esfericos 
-printf("coordenadas esfericas:\n);
-printf("%f%f%f\n", re, theta, phi):
+printf("coordenadas esfericas:\n");
+printf("%f%f%f\n", re, tetha, phi);
 //resultados cilindricos
-printf("coordenadas cilindricas")
-printf("%f%f%f\n", rc, theta, z)
+printf("coordenadas cilindricas");
+printf("%f%f%f\n", rc, tetha, z);
 exit(0);
 }
 
