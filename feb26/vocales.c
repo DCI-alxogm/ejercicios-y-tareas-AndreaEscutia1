e5811d9be09ea308158574275c9910a53473295c
AndreaEscutia1 <a.escutiazamudio@ugto.mx>
2025-02-26 11:54:17 -0600

    ejercicio

---
+++ feb26/vocales.c
@@ -0,0 +1,13 @@
+/* 
+ejercicio vocales
+*/
+
+#include<stdio.h>
+
+int main(){
+char c;
+int es_cocalminuscula, es_vocalmayuscula;
+printf("introduce una letra: ");
+scanf("%c", &c);
+es_vocalminuscula = (c==
+
