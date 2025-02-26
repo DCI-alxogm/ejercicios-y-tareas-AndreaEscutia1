f3085f445304efe0a0d4141d1a070c2c0a3ba68e
AndreaEscutia1 <a.escutiazamudio@ugto.mx>
2025-02-26 11:47:36 -0600

    primera parte

---
+++ feb26/par_impar.c
@@ -0,0 +1,24 @@
+/* 
+ejercicio 26/02/2025 Andrea Escutia 
+*/ 
+
+#include <stdio.h>
+int main(){
+char si,no;
+int numero,residuo;
+printf("Ingrese numero\n");
+scanf("%i",&numero);
+residuo=numero%2;
+if(residuo==0){
+printf("%i es par\n",numero);
+}else{
+printf("%i es impar\n",numero);
+}
+return(0);
+
+printf("¿quieres saber si otro numero es par o impar?(si/no)\n");
+scanf("%s",respuesta);
+ 
+}
+
+
