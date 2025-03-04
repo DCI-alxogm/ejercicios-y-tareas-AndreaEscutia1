7a8a2bd4bb0fc8f026a57f8902a534e724e079b1
AndreaEscutia1 <a.escutiazamudio@ugto.mx>
2025-03-04 17:09:56 -0600

    Update vocales.c
    revision

--- feb26/vocales.c
+++ feb26/vocales.c
@@ -6,8 +6,16 @@ ejercicio vocales
 
 int main(){
 char c;
-int es_cocalminuscula, es_vocalmayuscula;
+int vocalminuscula, vocalmayuscula;
 printf("introduce una letra: ");
 scanf("%c", &c);
-es_vocalminuscula = (c==
+vocalminuscula = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+vocalmayuscula = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
 
+if(vocalmayuscula || vocalminuscula){
+printf("%c es vocal\n", c);
+}else{
+printf("%c es una consonante\n", c);
+}
+return(0);
+}
