77e9da2974182d292aa42fb3b3cbb1ddd64ddd6c
AndreaEscutia1 <a.escutiazamudio@ugto.mx>
2025-03-31 11:50:53 -0600

    ejercico 2

---
+++ mar31/ejercicio2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+int inicial, final, incremento, x;
+
+printf("ingrese valor inicial de x\n");
+scanf("%d", inicial);
+printf("ingrese el valor final\n");
+scanf("%d", final);
+printf("ingrese el incremento\n");
+scanf("%d", incremento);
+
+x=inicial;
+printf
