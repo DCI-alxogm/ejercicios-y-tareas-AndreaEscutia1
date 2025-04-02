07b6c564c443314925d0783472593c31f0a3c800
AndreaEscutia1 <a.escutiazamudio@ugto.mx>
2025-04-02 01:05:56 -0600

    ejercicio 2

--- mar31/ejercicio2.c
+++ mar31/ejercicio2.c
@@ -4,11 +4,27 @@ int main(){
 int inicial, final, incremento, x;
 
 printf("ingrese valor inicial de x\n");
-scanf("%d", inicial);
+scanf("%d", &inicial);
 printf("ingrese el valor final\n");
-scanf("%d", final);
+scanf("%d", &final);
 printf("ingrese el incremento\n");
-scanf("%d", incremento);
+scanf("%d", &incremento);
+
+printf("ingrese el valor inicial de x:\n");
+scanf("%d", &inicial);
+
+printf("ingrese el valor final de x:\n");
+scanf("%d", &final);
+
+printf("ingrese el valor del  incremento:\n");
+scanf("%d", &incremento);
 
 x=inicial;
-printf
+printf("\nValores de X y X^2:\n");
+
+do{
+printf("x=%.2d -> X^2 =%.2d\n", x, x*x);
+x+= incremento;
+}while(x <= final);
+return 0;
+}
