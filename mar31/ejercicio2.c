b1d1c9b3720a2c54663b1f51b526e882232b6f12
AndreaEscutia1 <a.escutiazamudio@ugto.mx>
2025-04-02 10:33:07 -0600

    agregar regreso

--- mar31/ejercicio2.c
+++ mar31/ejercicio2.c
@@ -2,21 +2,13 @@
 
 int main(){
 int inicial, final, incremento, x;
+char opcion;
 
 printf("ingrese valor inicial de x\n");
 scanf("%d", &inicial);
 printf("ingrese el valor final\n");
 scanf("%d", &final);
 printf("ingrese el incremento\n");
-scanf("%d", &incremento);
-
-printf("ingrese el valor inicial de x:\n");
-scanf("%d", &inicial);
-
-printf("ingrese el valor final de x:\n");
-scanf("%d", &final);
-
-printf("ingrese el valor del  incremento:\n");
 scanf("%d", &incremento);
 
 x=inicial;
@@ -26,5 +18,11 @@ do{
 printf("x=%.2d -> X^2 =%.2d\n", x, x*x);
 x+= incremento;
 }while(x <= final);
+printf("quieres realizar otro calculo?(s/n)\n");
+scanf("%c", &opcion);
+while(opcion == 's');
+printf("programa finalizado\n");
+
 return 0;
+
 }
