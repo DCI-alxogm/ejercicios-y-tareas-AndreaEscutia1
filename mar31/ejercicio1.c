9e2e5254a8481165e062ca15bc5919516cc857a7
AndreaEscutia1 <a.escutiazamudio@ugto.mx>
2025-04-02 10:40:26 -0600

    agregar regreso

--- mar31/ejercicio1.c
+++ mar31/ejercicio1.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
 int main(){
+char opcion;
 float numero;
 do{
 printf("ingrese un numero\n");
@@ -8,6 +9,11 @@ scanf("%f", &numero);
 }
 while(numero<=0);{
 printf("numero ingresado correctamente\n");
+printf("¿Desea ingresar otro número? (s/n): ");
+scanf(" %c", &opcion);
+ while (opcion == 's');
+
+    printf("Programa finalizado.\n");
 }
 return 0;
 }
