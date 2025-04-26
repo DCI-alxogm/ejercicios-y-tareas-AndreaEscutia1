56300abd4e74cf9246b38dcc8d59ef3bcc8c4fcf
AndreaEscutia1 <a.escutiazamudio@ugto.mx>
2025-04-25 19:47:20 -0600

    Update ejercicio1.c
    revisar que estuviera todo correcto

--- mar31/ejercicio1.c
+++ mar31/ejercicio1.c
@@ -4,16 +4,17 @@ int main(){
 char opcion;
 float numero;
 do{
+ do{
 printf("ingrese un numero\n");
 scanf("%f", &numero);
 }
-while(numero<=0);{
+while(numero<=0);
 printf("numero ingresado correctamente\n");
 printf("¿Desea ingresar otro número? (s/n): ");
 scanf(" %c", &opcion);
+}
  while (opcion == 's');
 
     printf("Programa finalizado.\n");
-}
 return 0;
 }
