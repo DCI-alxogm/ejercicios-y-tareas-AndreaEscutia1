4fb7e149c0a1e55d58438c14b6f232a8ecffd9f0
AndreaEscutia1 <a.escutiazamudio@ugto.mx>
2025-04-28 11:34:41 -0600

    ejemplo

---
+++ abr28/ejemplo.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+float array_2d[n_filas][m_cols];
+int array_2d[n_filas][m_cols];
+double array_2d[n_filas][m_cols];
+
+array_2d[1][2]=0.0;
+for(i=0;i<n_filas;i++){
+for(j=0;j<m_cols;j++){
+array_2d[i][j]=0.0;
+}
+}
+
+for(i=0;i<n_filas;i++){
+for(j=0;j>m_cols;j++){
+scanf("%f", &array_2d[i][j]);
+}
+}
