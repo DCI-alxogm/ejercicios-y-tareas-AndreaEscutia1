7f47227ff3efee4de29be319ed772d6718919de7
AndreaEscutia1 <a.escutiazamudio@ugto.mx>
2025-06-01 23:45:08 -0600

    sin salida y entrada

---
+++ may26/ejercicio3.c
@@ -0,0 +1,11 @@
+//sin salida y entrada
+#include <stdio.h>
+
+void saludar() {
+    printf("¡Hola! Esta función no recibe ni devuelve nada.\n");
+}
+
+int main() {
+    saludar();
+    return 0;
+}
